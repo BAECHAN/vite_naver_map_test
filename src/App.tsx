8cb34a25796fb53b4b29f3ede28c632dcf5acfbe
c_bae <c_bae@dorosee.kr>
2024-05-04 19:42:35 +0900

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  <script
+    type="text/javascript"
+    src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${
+      import.meta.env.VITE_NAVER_MAP_API_KEY_ID
+    }&submodules=geocoder`}
+  ></script>;
+
+  return <></>;
+}
+
+export default App;
