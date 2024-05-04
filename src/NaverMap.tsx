2a07d8c5a8c53cd253db42ff318ec6a66a8af8d3
c_bae <c_bae@dorosee.kr>
2024-05-04 22:32:06 +0900

    feat: 내위치기준으로 지도 표출하고 marker 표시

--- src/NaverMap.tsx
+++ src/NaverMap.tsx
@@ -3,14 +3,43 @@ import React, { useEffect, useRef } from "react";
 const NaverMap: React.FC = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
+  // useEffect(() => {
+  //   if (mapRef.current) {
+  //     const mapOptions = {
+  //       center: new naver.maps.LatLng(37.3595704, 127.105399),
+  //       zoom: 13,
+  //     };
+
+  //     new naver.maps.Map(mapRef.current, mapOptions);
+  //   }
+  // }, []);
+
   useEffect(() => {
     if (mapRef.current) {
-      const mapOptions = {
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
-        zoom: 13,
-      };
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
 
-      new naver.maps.Map(mapRef.current, mapOptions);
+          const mapOptions = {
+            center: new naver.maps.LatLng(latitude, longitude),
+            zoom: 13,
+          };
+
+          const map = new naver.maps.Map(
+            mapRef.current as HTMLDivElement,
+            mapOptions
+          );
+
+          const markerOptions = {
+            position: new naver.maps.LatLng(latitude, longitude),
+            map: map,
+          };
+
+          const marker = new naver.maps.Marker(markerOptions);
+        });
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
     }
   }, []);
 
